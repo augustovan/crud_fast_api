4f5fb44bf465c6fd7e8f6091126efb4a2ae0762b
Victor Nascimento <augusto.van@gmail.com>
2025-01-02 17:54:28 -0300

    feat: create migration file

--- core/configs.py
+++ core/configs.py
@@ -1,6 +1,18 @@
-from pydantic import BaseSettings
+import os
+from dotenv import load_dotenv, find_dotenv
+from pydantic_settings import BaseSettings
 from sqlalchemy.ext.declarative import declarative_base
+from typing import ClassVar
 
+load_dotenv(find_dotenv())
+
+host = os.environ["POSTGRES_HOST"]
+port = os.environ["POSTGRES_PORT"]
+user = os.environ["POSTGRES_USER"]
+pw = os.environ["POSTGRES_PASSWORD"]
+db_name = os.environ["POSTGRES_DB"]
+
+DBBaseModel = declarative_base()
 
 class Settings(BaseSettings):
     """
@@ -8,10 +20,12 @@ class Settings(BaseSettings):
 
     """
     API_V1_STR: str = '/api/v1'
-    DB_URL: str = "postgresql+asyncpg://geek:university@localhost:5432/falculdade"
-    DBBaseModel = declarative_base()
+    DB_URL: str 
+    DBBaseModel: ClassVar = DBBaseModel 
 
     class Config:
         case_sensitive = True 
 
-settings = Settings()
+settings = Settings(
+    DB_URL = f"postgresql+asyncpg://{user}:{pw}@{host}:{port}/{db_name}"
+)
