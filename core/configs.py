2c0c5831ba4ff474af21b749ae3bc4c96c498c3c
Victor Nascimento <34478974+augustovan@users.noreply.github.com>
2025-01-02 19:33:27 -0300

    feat(001) - : Atualização API  (#1)
    * feat: create core files
    
    * feat: create models files
    
    * feat: create schemas files
    
    * feat: create migration file
    
    * feat: create api files
    
    * feat: create main api

---
+++ core/configs.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+from pydantic_settings import BaseSettings
+from sqlalchemy.ext.declarative import declarative_base
+from typing import ClassVar
+
+load_dotenv(find_dotenv())
+
+host = os.environ["POSTGRES_HOST"]
+port = os.environ["POSTGRES_PORT"]
+user = os.environ["POSTGRES_USER"]
+pw = os.environ["POSTGRES_PASSWORD"]
+db_name = os.environ["POSTGRES_DB"]
+
+DBBaseModel = declarative_base()
+
+class Settings(BaseSettings):
+    """
+    Configurações gerais usadas na aplicação
+
+    """
+    API_V1_STR: str = '/api/v1'
+    DB_URL: str 
+    DBBaseModel: ClassVar = DBBaseModel 
+
+    class Config:
+        case_sensitive = True 
+
+settings = Settings(
+    DB_URL = f"postgresql+asyncpg://{user}:{pw}@{host}:{port}/{db_name}"
+)
